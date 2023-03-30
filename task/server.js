c01f8e323e330744a58bb320946e5dd43c4b5a73
Ritik-itobuz <ritik@itobuz.com>
2023-03-30 04:02:18 -0700

    updated

--- task/server.js
+++ task/server.js
@@ -3,32 +3,49 @@ const port = 8000;
 const path = require("path");
 const fs = require("fs/promises");
 const fsx = require("fs");
+const  {parse}  = require("querystring")
 
 const filePath = path.join(__dirname + "/db.json");
 const data = JSON.parse(fsx.readFileSync(filePath, "utf-8"));
+
+async function writeFormData(formData) {
+  const dataFromFile = await fs.readFile("./data.txt", "utf8");
+  const fileData = JSON.parse(dataFromFile)
+  fileData.push(formData)
+  await fs.writeFile("./data.txt", JSON.stringify(fileData));
+}
 
 http
   .createServer((req, res) => {
     try {
       res.setHeader("Access-Control-Allow-Origin", "*");
       res.setHeader("Access-Control-Allow-Headers", "*");
-      const formData = [];
-      req.on("data", (formDataPieces) => {
-        formData.push(formDataPieces);
+      let formData = "";
+      req.on("data", (formDataPeices) => {
+        formData = formDataPeices.toString();
       });
       req.on("end", () => {
-        console.log(Buffer.concat(formData).toString());
-
-        let formdata = Buffer.concat(formData).toString();
-
-        let getData = async () => {
-          
-          let presentData = await fs.writeFile("./data.txt", formData);
-         let  formDataNew = formData + ";" + presentData;
-          await fs.writeFile("./data.txt", formDataNew);
-        };
-        getData();
+        if (Object.keys(formData).length !== 0) {
+          writeFormData(parse(formData))
+        }
       });
+      // const formData = [];
+      // req.on("data", (formDataPieces) => {
+      //   formData.push(formDataPieces);
+      // });
+      // req.on("end", () => {
+        // console.log(Buffer.concat(formData).toString());
+
+        // let formdata = Buffer.concat(formData).toString();
+
+        // let getData = async () => {
+          
+        //   let presentData = await fs.writeFile("./data.txt", formData);
+        //  let  formDataNew = formData + ";" + presentData;
+        //   await fs.writeFile("./data.txt", formDataNew);
+        //};
+        //getData();
+      //});
       
       res.end(JSON.stringify(data));
     } catch (err) {
